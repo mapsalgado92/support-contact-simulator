2e26309e6d13129fcc4bda8f42cb7e0620aaa446
Mario Salgado <mario.salgado@revolut.com>
2023-07-07 01:38:50 +0000

    change package name

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='support-contact-simulations',
     version='1.0.0',
     description='Support Contact Simulations',
-    packages=['concurrency-simulator'],
+    packages=['concurrency_simulator'],
     install_requires=['numpy'],
 )
